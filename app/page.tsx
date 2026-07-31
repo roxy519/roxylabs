641775e1fddba8691af5976b5586fb05f9b6c36b
roxy519 <roxanne.bischoff@gmail.com>
2026-07-31 13:29:38 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-[#353940] font-mono text-white">
+    <div className="flex flex-col flex-1 items-center justify-center bg-[#262424] font-mono text-white">
     <main>
       <h1>roxy labs</h1>
       <p>experiments in ai, automation, marketing, operations, and creative projects</p>
