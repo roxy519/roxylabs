75bcdb69768f98f1e08d448f813de44ec82db2c3
roxy519 <roxanne.bischoff@gmail.com>
2026-07-31 15:52:39 -0400

    favi

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-[#pwd262424] font-mono text-white">
+    <div className="flex flex-col flex-1 items-center justify-center bg-[#242526] font-mono text-white">
   <main className="min-h-screen flex items-center justify-center">
   <div className="flex items-center gap-8">
     <Image
